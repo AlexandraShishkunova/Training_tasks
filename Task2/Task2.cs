b559e95743c8e8c5d22c96369a95162f7bb4e4ba
Alexandra Shishkunova <sa.shaa13776@gmail.com>
2023-10-11 15:51:04 +0300

    Task2

---
+++ Task2/Task2.cs
@@ -0,0 +1,20 @@
+﻿//На вводе будут числа. Тебе нужно преобразовать все в массив и вывести массив на консоль.
+
+Console.WriteLine("Введите количество чисел: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[] numbers = new int[n];
+for (int i = 0; i < n; i++)
+{
+    Console.WriteLine("\nВведите число: ");
+    int number = Convert.ToInt32(Console.ReadLine());
+    numbers[i] = number;
+}
+
+Console.WriteLine("\nВывод: ");
+foreach (int item in numbers)
+{
+    Console.WriteLine(item);
+}
+
+
+
---
+++ Task3/Task3.cs
@@ -0,0 +1,17 @@
+﻿//На вводе одно слово, удали из него все буквы "А" Выведи это слово
+
+Console.WriteLine("Введите одно слово: ");
+string word = Console.ReadLine();
+
+string[] words = word.Split(' ');
+char[] chars = word.ToCharArray();
+
+if (words.Length==1)
+{
+    chars = Array.FindAll(chars, c => c != 'A' && c != 'a');
+    foreach(char i in chars)
+    Console.WriteLine(i);
+}
+   
+else
+    Console.WriteLine("Необходимо ввести одно слово!");
